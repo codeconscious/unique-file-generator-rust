dffa300de542864deea53b3a7fc9399a4cc4971a
CodeConscious <hello@codeconscio.us>
2023-02-28 13:57:25 +0900

    Add indicatif crate

--- README.md
+++ README.md
@@ -1,8 +1,5 @@
 # Unique File Generator (written in Rust)
 
-Rewriting my [C# unique file generator](https://github.com/codeconscious/unique-file-generator) in Rust as a way to get more familiar with Rust. I'm just getting underway, though, so there's nothing much to see—let alone use—yet.
+Rewriting my [C# unique file generator](https://github.com/codeconscious/unique-file-generator) in Rust as a way to get more familiar with Rust. Most of the features have been merged, and it's now functional.
 
-## TODO
-
-- [ ] Reimplement all features of the C# tool
-- [ ] Likely add [indicatif](https://github.com/console-rs/indicatif)
+More to come...
--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,12 @@ use arg_parsing::parse_args;
 use colored::*;
 use random_strings::random_alphanumeric_string;
 
+use std::thread;
+use std::time::Duration;
+use std::{cmp::min, fmt::Write};
+
+use indicatif::{ProgressBar, ProgressState, ProgressStyle};
+
 fn main() {
     let args = parse_args();
     match &args {
@@ -17,8 +23,22 @@ fn main() {
     };
 
     let safe_args = args.unwrap();
+    let total_size = safe_args.count as u64;
+    let pb = ProgressBar::new(total_size);
+    pb.set_style(ProgressStyle::with_template("{spinner:.green} [{elapsed_precise}] [{wide_bar:.cyan/blue}] {bytes}/{total_bytes} ({eta})")
+        .unwrap()
+        .with_key("eta", |state: &ProgressState, w: &mut dyn Write| write!(w, "{:.1}s", state.eta().as_secs_f64()).unwrap())
+        .progress_chars("#>-"));
 
-    for _ in 0..safe_args.count {
+    // while downloaded < total_size {
+
+    // }
+
+    for i in 0..safe_args.count {
+        let new = min(i, safe_args.count) as u64;
+        pb.set_position(new);
+        thread::sleep(Duration::from_millis(1000));
+
         let filename_base = random_alphanumeric_string(20);
         let filename = safe_args.full_filename(&filename_base);
         let file_body = match safe_args.size {
@@ -37,4 +57,6 @@ fn main() {
             Err(e) => println!("{}", e.to_string().red()),
         }
     }
+
+    pb.finish_with_message("Done!");
 }
