2d0b67f917993c76e4f9e380dfa660d32cbdd795
CodeConscious <hello@codeconscio.us>
2023-02-28 17:35:26 +0900

    Add progress indicator (#5)
    * Add indicatif crate
    
    * Various tidying up

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,13 @@ mod arg_parsing;
 mod generate_files;
 mod random_strings;
 
-use arg_parsing::parse_args;
+use arg_parsing::{parse_args, Arguments};
 use colored::*;
+use indicatif::{ProgressBar, ProgressState, ProgressStyle};
 use random_strings::random_alphanumeric_string;
+use std::thread;
+use std::time::Duration;
+use std::{cmp::min, fmt::Write};
 
 fn main() {
     let args = parse_args();
@@ -17,18 +21,19 @@ fn main() {
     };
 
     let safe_args = args.unwrap();
+    let progress_bar = prepare_progress_bar(&safe_args);
 
-    for _ in 0..safe_args.count {
+    for i in 0..safe_args.count {
+        let new = min(i, safe_args.count) as u64;
+        progress_bar.set_position(new);
+        thread::sleep(Duration::from_millis(500)); // TODO: Remove once `sleep` is added.
+
         let filename_base = random_alphanumeric_string(20);
         let filename = safe_args.full_filename(&filename_base);
         let file_body = match safe_args.size {
             Some(s) => random_alphanumeric_string(s),
             None => filename.clone(),
         };
-
-        // dbg!("{}", i);
-        // dbg!("- Full filename: {}", &filename);
-        // dbg!("- Body contents: {}", &file_body);
 
         let write_result =
             generate_files::create_file(filename, file_body, safe_args.subdirectory.clone());
@@ -37,4 +42,23 @@ fn main() {
             Err(e) => println!("{}", e.to_string().red()),
         }
     }
+
+    progress_bar.finish_with_message("Done!");
+}
+
+fn prepare_progress_bar(args: &Arguments) -> ProgressBar {
+    let total_size = args.count as u64;
+
+    let progress_bar = ProgressBar::new(total_size);
+    progress_bar.set_style(
+        ProgressStyle::with_template(
+            "{spinner:.green} [{elapsed_precise}] [{wide_bar:.cyan/blue}] {percent}% ({eta})",
+        )
+        .unwrap()
+        .with_key("eta", |state: &ProgressState, w: &mut dyn Write| {
+            write!(w, "{:.1}s", state.eta().as_secs_f64()).unwrap()
+        })
+        .progress_chars("#>-"),
+    );
+    progress_bar
 }
