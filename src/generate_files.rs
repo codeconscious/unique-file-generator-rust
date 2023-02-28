e3fa4e9fd8691b15b2372cef48ab443c2ee0d662
CodeConscious <hello@codeconscio.us>
2023-02-28 20:06:49 +0900

    Add delay option (#6)
    * Various tidying and renaming; cleanup
    
    * Improve path handling; various cleanup
    
    * Fix version

--- src/generate_files.rs
+++ src/generate_files.rs
@@ -1,18 +1,30 @@
 use std::fs;
 use std::fs::File;
 use std::io::prelude::*;
-
-fn prepare_subdirectory(directory_name: &str) -> std::io::Result<()> {
-    fs::create_dir_all(directory_name)?;
-    Ok(())
-}
+use std::path::Path;
 
 pub fn create_file(filename: String, content: String, subfolder: String) -> std::io::Result<()> {
     prepare_subdirectory(&subfolder)?;
 
-    let path = subfolder + "/" + filename.as_ref(); // TODO: OS-specific separator?
-    let mut file = File::create(path).expect("Error encountered while creating file!");
-    file.write_all(content.as_bytes())
-        .expect("Error while writing content to file!");
+    let path = Path::new(".").join(subfolder).join(filename);
+    let mut file = File::create(&path).expect(
+        format!(
+            "Error encountered while creating file \"{}\"!",
+            path.to_string_lossy()
+        )
+        .as_str(),
+    );
+    file.write_all(content.as_bytes()).expect(
+        format!(
+            "Error while writing content to file \"{}\"!",
+            path.to_string_lossy()
+        )
+        .as_str(),
+    );
+    Ok(())
+}
+
+fn prepare_subdirectory(directory_name: &str) -> std::io::Result<()> {
+    fs::create_dir_all(directory_name)?;
     Ok(())
 }
--- src/random_strings.rs
+++ src/random_strings.rs
@@ -9,11 +9,3 @@ pub fn random_alphanumeric_string(length: usize) -> String {
         .map(char::from)
         .collect();
 }
-
-// pub fn random_string(length: usize, char_bank: &str) -> String {
-//     return thread_rng()
-//         .sample_iter(&Alphanumeric)
-//         .take(length)
-//         .map(char::from)
-//         .collect();
-// }
