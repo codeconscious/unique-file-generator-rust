51dfa1caf312542bcc86e479e8de7218ccca032a
CodeConscious <hello@codeconscio.us>
2023-02-28 09:31:28 +0900

    Add directory creation; minor tweaks

--- src/generate_files.rs
+++ src/generate_files.rs
@@ -1,9 +1,17 @@
+use std::fs;
 use std::fs::File;
 use std::io::prelude::*;
 
-pub fn create_file(filename: String, content: String, subfolder: String) -> Result<(), String> {
-    // let path = subfolder + "/" + filename.as_ref(); // TODO: OS-specific separator?
-    let mut file = File::create(filename).expect("Error encountered while creating file!");
+fn prepare_subdirectory(directory_name: &str) -> std::io::Result<()> {
+    fs::create_dir_all(directory_name)?;
+    Ok(())
+}
+
+pub fn create_file(filename: String, content: String, subfolder: String) -> std::io::Result<()> {
+    prepare_subdirectory(&subfolder)?;
+
+    let path = subfolder + "/" + filename.as_ref(); // TODO: OS-specific separator?
+    let mut file = File::create(path).expect("Error encountered while creating file!");
     file.write_all(content.as_bytes())
         .expect("Error while writing content to file!");
     Ok(())
